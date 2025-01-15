18ec58a90985735720a9ff5770231ed3707d4d09
Ahana Chandra <ahanac3@nycstudents.net>
2025-01-15 09:45:21 -0500

    fixed support others

--- Fairy.java
+++ Fairy.java
@@ -66,7 +66,7 @@ public class Fairy extends Adventurer{
     }
     /*Restores 5 special to other*/
     public String support(Adventurer other){
-      return "Gives a coffee to "+other+" and restores "
+      return "Spells "+other+" with "
       + other.restoreSpecial(5)+" "+other.getSpecialName();
     }
     /*Restores 6 special and 1 hp to self.*/
