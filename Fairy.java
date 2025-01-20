f5dfd7158d197724799ce9d521a27999bfa2a42c
Ahana Chandra <ahanac3@nycstudents.net>
2025-01-20 09:17:18 -0500

    fixed supports and attacks in Fairy

--- Fairy.java
+++ Fairy.java
@@ -31,7 +31,7 @@ public class Fairy extends Adventurer{
     }
 
     public void setSpecial(int n){
-      fairyDust = n;
+      fairyDust = Math.max(0, Math.min(n, fairyDustMax));
     }
 
     public int getSpecialMax(){
@@ -42,8 +42,8 @@ public class Fairy extends Adventurer{
     public String attack(Adventurer other){
       int damage = (int)(Math.random()*5)+4;
       other.applyDamage(damage);
-      setHP(this.getHP() + 3);
-      return this + " spelled "+ other + " and dealt "+ damage +
+      setHP(Math.min(getmaxHP(), this.getHP() + 3));
+      return getName() + " spelled "+ other.getName() + " and dealt "+ damage +
       " points of damage. They then take a bite of a healing huckleberry.";
     }
 
@@ -51,10 +51,10 @@ public class Fairy extends Adventurer{
     public String specialAttack(Adventurer other){
       if(getSpecial() >= 5){
         setSpecial(getSpecial() - 5);
-        other.setSpecial(getSpecial() - 10);
-        return this + " used their "+preferredWandType+
-        " wand to glitter-bomb "+ other + "." +
-        " This blasted "+other+", decreasing their "+ other.getSpecialName() +" by 10.";
+        other.setSpecial(other.getSpecial() - 10);
+        return getName() + " used their "+preferredWandType+
+        " wand to glitter-bomb "+ other.getName() + "." +
+        " This blasted "+other.getName()+", decreasing their "+ other.getSpecialName() +" by 10.";
       }else{
         return "Not enough fairyDust to power the glitter-bomb. Instead "+attack(other);
       }
@@ -62,13 +62,13 @@ public class Fairy extends Adventurer{
     }
 
     public String support(Adventurer other){
-      return "Spells "+other+" with "
-      + other.restoreSpecial(5)+" "+other.getSpecialName();
+      int restored = other.restoreSpecial(5);
+      return getName() + " spells " + other.getName() + " with " + restored + " " + other.getSpecialName() + ".";
     }
 
     public String support(){
-      return this+" travels to the Magic Forest to obtain "+restoreSpecial(5)+" "
-      + getSpecialName();
+      int restored = restoreSpecial(5);
+      return getName() + " travels to the Magic Forest to obtain " + restored + " " + getSpecialName() + ".";
     }
 
   }
